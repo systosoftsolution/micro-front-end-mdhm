ad495397117c5b099de76410554c87490ce4ad16
MD <46341074+systosoft@users.noreply.github.com>
2022-01-20 11:43:04 +0530

    Update webpack.config.js

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:8000/",
+    publicPath: "https://main.d5ho35mbjfdex.amplifyapp.com:8000/",
   },
 
   resolve: {
